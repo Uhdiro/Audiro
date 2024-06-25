dda877cd5fecab70dd4e87001d0d484119cb369f
Jeonghee Lee <tpqms316@naver.com>
2024-06-25 17:31:26 +0900

    review-mapper.xml , ReviewController, ReviewService, (post-mapper, postService remove)

--- audiro/src/main/java/com/audiro/service/ReviewService.java
+++ audiro/src/main/java/com/audiro/service/ReviewService.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor  // final 필드들을 초기화하는 아규먼트를 갖는 생성자.
-public class PostService {
+public class ReviewService {
 	
 	private final PostDao postDao;
 
