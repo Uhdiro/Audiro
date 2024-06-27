f336297e1c21852b15dcdeec76d5e98fe017e2c5
sveiin <veiin9230@gmail.com>
2024-06-27 11:57:09 +0900

    ID and Password Validation

--- audiro/src/main/webapp/static/js/signup.js
+++ audiro/src/main/webapp/static/js/signup.js
@@ -23,19 +23,28 @@ document.addEventListener('DOMContentLoaded', () => {
     
     function checkId(event) {
         const id = event.target.value;
+        const checkIdResult = document.querySelector('div#checkIdResult');
+        const idRegex = /^[a-zA-Z0-9._]{5,15}$/;
+        
+        if (!idRegex.test(id)) {
+            checkIdResult.innerHTML = '영문자, 숫자, 밑줄 및 점을 포함할 수 있으며, 길이가 5자에서 15자 사이여야 합니다.';
+            checkIdResult.classList.add('text-danger');
+            checkIdResult.classList.remove('text-success');
+            return;
+        }
         
         const uri = `../api/user/check-id?id=${id}`;
+        
         axios
         .get(uri)
         .then((response) => {
-            const checkIdResult = document.querySelector('div#checkIdResult');
             
             if (response.data === 'Y') {
-                checkIdResult.innerHTML = '사용할 수 있는 아이디입니다.'
+                checkIdResult.innerHTML = '사용할 수 있는 아이디입니다.';
                 checkIdResult.classList.add('text-success');
                 checkIdResult.classList.remove('text-danger');
             } else {
-                checkIdResult.innerHTML = '사용할 수 없는 아이디입니다.'
+                checkIdResult.innerHTML = '사용할 수 없는 아이디입니다.';
                 checkIdResult.classList.add('text-danger');
                 checkIdResult.classList.remove('text-success');
             }
@@ -48,19 +57,27 @@ document.addEventListener('DOMContentLoaded', () => {
     function checkPassword() {
         const password = inputPassword.value;
         const confirmPassword = inputConfirmPassword.value;
-        
         const checkPasswordResult = document.querySelector('div#checkPasswordResult');
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
         
         if (password === '' || confirmPassword === '') {
+            checkPasswordResult.innerHTML = '';
+            return;
+        }
+        
+        if (!passwordRegex.test(password)) {
+            checkPasswordResult.innerHTML = '최소 8자 이상이며, 소문자, 대문자, 숫자, 특수문자가 각각 하나 이상 포함되어야 합니다.';
+            checkPasswordResult.classList.add('text-danger');
+            checkPasswordResult.classList.remove('text-success');
             return;
         }
         
         if (password === confirmPassword) {
-            checkPasswordResult.innerHTML = '비밀번호가 일치합니다.'
+            checkPasswordResult.innerHTML = '비밀번호가 일치합니다.';
             checkPasswordResult.classList.add('text-success');
             checkPasswordResult.classList.remove('text-danger');
         } else {
-            checkPasswordResult.innerHTML = '비밀번호가 일치하지 않습니다.'
+            checkPasswordResult.innerHTML = '비밀번호가 일치하지 않습니다.';
             checkPasswordResult.classList.add('text-danger');
             checkPasswordResult.classList.remove('text-success');
         }
