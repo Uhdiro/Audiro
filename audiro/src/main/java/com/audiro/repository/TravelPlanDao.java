f3283586fc8ce2341ebd2771552f3ed13731d022
saku <saku3709@gmail.com>
2024-07-08 10:45:59 +0900

    Comment: 주석 삭제

--- audiro/src/main/java/com/audiro/repository/TravelPlanDao.java
+++ audiro/src/main/java/com/audiro/repository/TravelPlanDao.java
@@ -1,16 +1,13 @@
 package com.audiro.repository;
 
-import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
-
-import com.audiro.dto.DetailedPlanDto;
 
 public interface TravelPlanDao {
 
 	Long insertTravelPlan(@Param("plan") TravelPlan plan);
 	
-	int insertDetailedPlan(DetailedPlan detailedPlans);
+	int insertDetailedPlan(DetailedPlan detailedPlan);
 	
 
 }
